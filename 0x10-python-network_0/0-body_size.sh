e4a5706c40f6a23e6ab681ea580a9a9d5573279e
Donald-Edinam <kobbyedinamdonald@gmail.com>
2024-03-05 16:55:42 +0000

    nee solution found

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,13 +1,3 @@
 #!/bin/bash
-# send a request to an URL with curl, and displays the size of the body of the response
-if [ -z "$1" ]; then
-    echo "Error: URL not provided."
-    exit 1
-fi
-
-response=$(curl -s "$1")
-if [ $? -ne 0 ]; then
-    echo "Error: Unable to fetch the content from the URL."
-    exit 1
-fi
-echo "$response" | wc -c
+# Displaying the results of GET method
+curl -sI "$1" | grep -i 'Content-Length' | awk '{print $2}'

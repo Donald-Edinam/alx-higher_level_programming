290b87d78caee77c15ad976ecff8dbdffc7515ff
Donald-Edinam <kobbyedinamdonald@gmail.com>
2024-03-05 17:02:47 +0000

    New ads

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,21 @@
 #!/bin/bash
-# Displaying the results of GET method
-curl -sI "$1" | grep -i 'Content-Length' | awk '{print $2}'
+
+# Check if correct number of arguments supplied
+if [[ "$#" -ne 1 ]]; then
+    echo "Usage: $0 <URL>" >&2
+    exit 1
+fi
+
+url="$1"
+
+# Use --head instead of -I to avoid auto-following redirects
+content_length=$(curl --silent --head "$url" \
+                      | awk '/^Content-Length:/{ print int($2) }')
+
+# Print result with unit suffixes
+if [[ -n "$content_length" ]]; then
+    printf "%d bytes\n" "$content_length"
+else
+    echo "Could not determine content length." >&2
+    exit 1
+fi

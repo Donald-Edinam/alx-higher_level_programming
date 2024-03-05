5140efaf6f6ba75991682da7a922ac252a429c05
Donald-Edinam <kobbyedinamdonald@gmail.com>
2024-03-05 16:48:51 +0000

    Code modified

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 # send a request to an URL with curl, and displays the size of the body of the response
-curl -s "$1" | wc -c
+if [ -z "$1" ]; then
+    echo "Error: URL not provided."
+    exit 1
+fi
+
+response=$(curl -s "$1")
+if [ $? -ne 0 ]; then
+    echo "Error: Unable to fetch the content from the URL."
+    exit 1
+fi
+echo "$response" | wc -c

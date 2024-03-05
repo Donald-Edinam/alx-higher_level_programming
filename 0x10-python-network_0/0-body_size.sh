ee77d40615113a48020e19bd217772af52b67898
Donald-Edinam <kobbyedinamdonald@gmail.com>
2024-03-05 18:51:16 +0000

    Hard work pays

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,21 +1,3 @@
 #!/bin/bash
-
-# Check if correct number of arguments supplied
-if [[ "$#" -ne 1 ]]; then
-    echo "Usage: $0 <URL>" >&2
-    exit 1
-fi
-
-url="$1"
-
-# Use --head instead of -I to avoid auto-following redirects
-content_length=$(curl --silent --head "$url" \
-                      | awk '/^Content-Length:/{ print int($2) }')
-
-# Print result with unit suffixes
-if [[ -n "$content_length" ]]; then
-    printf "%d bytes\n" "$content_length"
-else
-    echo "Could not determine content length." >&2
-    exit 1
-fi
+# send a request to an URL with curl, and displays the size of the body of the response
+curl -s "$1" | wc -c
